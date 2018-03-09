c30448eeba5a28392806dbaee99b72c4ad26e3c2
jagjeet0824 <jagjeet0824@gmail.com>
2018-03-09 14:38:28 -0500

    first commit

---
+++ Models/FlightType.cs
@@ -0,0 +1,28 @@
+namespace DotNetMidtermA.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class FlightType
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public FlightType()
+        {
+            Flights = new HashSet<Flight>();
+        }
+
+        public int FlightTypeId { get; set; }
+
+        [Column("FlightType")]
+        [Required]
+        [StringLength(50)]
+        [Display(Name ="Type")]
+        public string FlightType1 { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Flight> Flights { get; set; }
+    }
+}
